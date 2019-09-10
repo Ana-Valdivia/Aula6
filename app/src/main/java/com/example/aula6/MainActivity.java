9b685831eb4bdf314968e44801766e126d3787b9
ana-valdivia <ana.raquel1291@hotmail.com>
2019-09-09 21:54:22 -0300

    Aula-6

---
+++ app/src/main/java/com/example/aula6/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.aula6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void next(View view) {
+        Intent seg2 =new Intent(this,SegundaActivity.class);
+        startActivity(seg2);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/aula6/SegundaActivity.java
@@ -0,0 +1,18 @@
+package com.example.aula6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+
+
+    public void back(View view) {
+    }
+}
